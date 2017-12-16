6e01c73dd2f84c71d76cfcbc5b2c6b84320d680b
AndreeaBila <andreeabila@yahoo.com>
2017-12-17 01:15:20 +0200

    footer js

--- js/footer.js
+++ js/footer.js
@@ -15,4 +15,42 @@ $(function() {
     window.location = 'profile.php';
   });
 
+  if(window.location.href == 'http://localhost/WebDev/ZirafApp/php/chat.php') {
+    console.log("yes");
+    $('#newsfeedNav').removeClass('activeNav');
+    $('#profileNav').removeClass('activeNav');
+    $('#reviewNav').removeClass('activeNav');
+    $('#chatNav').addClass('activeNav');
+  } 
+
+  switch(window.location.href) {
+    case 'http://localhost/WebDev/ZirafApp/php/newsfeed.php':
+      $('#chatNav').removeClass('activeNav');
+      $('#profileNav').removeClass('activeNav');
+      $('#reviewNav').removeClass('activeNav');
+      $('#newsfeedNav').addClass('activeNav');
+      break;
+
+    case 'http://localhost/WebDev/ZirafApp/php/chat.php':
+      $('#newsfeedNav').removeClass('activeNav');
+      $('#profileNav').removeClass('activeNav');
+      $('#reviewNav').removeClass('activeNav');
+      $('#chatNav').addClass('activeNav');
+      break;
+
+    case 'http://localhost/WebDev/ZirafApp/php/review.php':
+      $('#newsfeedNav').removeClass('activeNav');
+      $('#profileNav').removeClass('activeNav');
+      $('#chatNav').removeClass('activeNav');
+      $('#reviewNav').addClass('activeNav');
+      break;
+
+    case 'http://localhost/WebDev/ZirafApp/php/profile.php':
+      $('#newsfeedNav').removeClass('activeNav');
+      $('#chatNav').removeClass('activeNav');
+      $('#reviewNav').removeClass('activeNav');
+      $('#profileNav').addClass('activeNav');
+      break;
+  }
+  
 });
--- php/phpComponents/footer.php
+++ php/phpComponents/footer.php
@@ -3,17 +3,20 @@
     <div class="row">
       <button class="col-3 text-center" id="newsfeedNav">
         <p><i class="fa fa-bell" aria-hidden="true"></i></p>
+        <p class="navText">Newsfeed</p>
       </button>
       <button class="col-3 text-center" id="chatNav">
         <p><i class="fa fa-comments" aria-hidden="true"></i></p>
+        <p class="navText">Chat</p>
       </button>
       <button class="col-3 text-center" id="reviewNav">
         <p><i class="fa fa-file-text" aria-hidden="true"></i></p>
+        <p class="navText">Review</p>
       </button>
       <button class="col-3 text-center" id="profileNav">
         <p><i class="fa fa-user" aria-hidden="true"></i></p>
+        <p class="navText">Profile</p>
       </button>
-
     </div>
 
 </footer>

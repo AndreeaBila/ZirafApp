ccb728fc6d1be29e4af4e06ec037cfce3a9cd26a
AndreeaBila <andreeabila@yahoo.com>
2017-12-17 00:48:13 +0200

    nf chat profile review footer header

---
+++ js/footer.js
@@ -0,0 +1,18 @@
+$(function() {
+  $('#newsfeedNav').click(function() {
+    window.location = 'newsfeed.php';
+  });
+
+  $('#chatNav').click(function() {
+    window.location = 'chat.php';
+  });
+
+  $('#reviewNav').click(function() {
+    window.location = 'review.php';
+  });
+
+  $('#profileNav').click(function() {
+    window.location = 'profile.php';
+  });
+
+});
---
+++ php/phpComponents/footer.php
@@ -0,0 +1,23 @@
+<footer class="footer" id="footer">
+
+    <div class="row">
+      <button class="col-3 text-center" id="newsfeedNav">
+        <p><i class="fa fa-bell" aria-hidden="true"></i></p>
+      </button>
+      <button class="col-3 text-center" id="chatNav">
+        <p><i class="fa fa-comments" aria-hidden="true"></i></p>
+      </button>
+      <button class="col-3 text-center" id="reviewNav">
+        <p><i class="fa fa-file-text" aria-hidden="true"></i></p>
+      </button>
+      <button class="col-3 text-center" id="profileNav">
+        <p><i class="fa fa-user" aria-hidden="true"></i></p>
+      </button>
+
+    </div>
+
+</footer>
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" 
+    integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<!-- The js script for this file -->
+<script src="../js/footer.js"></script>

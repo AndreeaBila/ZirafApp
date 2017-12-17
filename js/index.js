a883e5abdf595d146a2087d78420080fc438af96
SpecDI <vlad@herghelegiu.com>
2017-12-17 20:46:20 +0200

    plm

--- js/index.js
+++ js/index.js
@@ -37,6 +37,14 @@ function signupVerification(){
   if(!checkUniqueEmail()){
     alert("Email not unique");
     return false;
+  }else if($('#signupPassword').val() != $('#signupConfirmPassword').val()){
+    //check if the two passwords are equal
+    alert("The two passwords do not match");
+    return false;
+  }else if(!checkFile()){
+    //check if the submitted file is of an accepted file type
+    alert("Submitted file type is not correct");
+    return false;
   }
   return true;
 }
@@ -56,4 +64,15 @@ function checkUniqueEmail(){
     }
   });
   return result;
+}
+
+function checkFile(){
+  //check the type of the uploaded file and make sure it is an image
+  var file = $('#signupProfilePictureBtn')[0].files[0];
+  var fileType = file["type"];
+  var ValidImageTypes = ["image/gif", "image/jpeg", "image/png", "image/jpg"];
+  if ($.inArray(fileType, ValidImageTypes) < 0) {
+       return false;
+  }
+  return true;
 }

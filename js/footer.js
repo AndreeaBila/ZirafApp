1c5764c5dc3ab2c2455446037bceaee9f21c7339
AndreeaBila <andreeabila@yahoo.com>
2017-12-17 02:41:26 +0200

    .php removed

--- js/footer.js
+++ js/footer.js
@@ -1,23 +1,23 @@
 $(function() {
   
   $('#newsfeedNav').click(function() {
-    window.location = 'newsfeed.php';
+    window.location = 'newsfeed';
   });
 
   $('#profileNav').click(function() {
-    window.location = 'profile.php';
+    window.location = 'profile';
   });
 
   $('#reviewNav').click(function() {
-    window.location = 'review.php';
+    window.location = 'review';
   });
 
   $('#chatNav').click(function() {
-    window.location = 'chat.php';
+    window.location = 'chat';
   });
 
 
-  if(window.location.href == 'http://localhost/WebDev/ZirafApp/php/chat.php') {
+  if(window.location.href == 'http://localhost/WebDev/ZirafApp/php/chat') {
     console.log("yes");
     $('#newsfeedNav').removeClass('activeNav');
     $('#profileNav').removeClass('activeNav');
@@ -26,28 +26,28 @@ $(function() {
   } 
 
   switch(window.location.href) {
-    case 'http://localhost/WebDev/ZirafApp/php/newsfeed.php':
+    case 'http://localhost/WebDev/ZirafApp/php/newsfeed':
       $('#chatNav').removeClass('activeNav');
       $('#profileNav').removeClass('activeNav');
       $('#reviewNav').removeClass('activeNav');
       $('#newsfeedNav').addClass('activeNav');
       break;
 
-    case 'http://localhost/WebDev/ZirafApp/php/chat.php':
+    case 'http://localhost/WebDev/ZirafApp/php/chat':
       $('#newsfeedNav').removeClass('activeNav');
       $('#profileNav').removeClass('activeNav');
       $('#reviewNav').removeClass('activeNav');
       $('#chatNav').addClass('activeNav');
       break;
 
-    case 'http://localhost/WebDev/ZirafApp/php/review.php':
+    case 'http://localhost/WebDev/ZirafApp/php/review':
       $('#newsfeedNav').removeClass('activeNav');
       $('#profileNav').removeClass('activeNav');
       $('#chatNav').removeClass('activeNav');
       $('#reviewNav').addClass('activeNav');
       break;
 
-    case 'http://localhost/WebDev/ZirafApp/php/profile.php':
+    case 'http://localhost/WebDev/ZirafApp/php/profile':
       $('#newsfeedNav').removeClass('activeNav');
       $('#chatNav').removeClass('activeNav');
       $('#reviewNav').removeClass('activeNav');

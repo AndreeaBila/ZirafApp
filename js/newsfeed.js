e37ea4eaab5e904b75f6439b74de9b525e57eea9
AndreeaBila <andreeabila@yahoo.com>
2017-12-29 19:54:55 +0200

    newsfeed

--- js/newsfeed.js
+++ js/newsfeed.js
@@ -1,3 +1,23 @@
 $(function() {
-  $('.your-checkbox').prop('indeterminate', true);
+  $('#writeAnnouncement').hide();
+  $('#uploadPicture').hide();
+  $('#createPoll').hide();
+
+  $('#openAnnouncementBoxBtn').click(function() {
+    $('#uploadPicture').hide();
+    $('#createPoll').hide();
+    $('#writeAnnouncement').toggle('slide',{direction:'up'},500);
+  });
+
+  $('#openPictureBoxBtn').click(function() {
+    $('#writeAnnouncement').hide();
+    $('#createPoll').hide();
+    $('#uploadPicture').toggle('slide',{direction:'up'},500);
+  });
+
+  $('#openPollBoxBtn').click(function() {
+    $('#uploadPicture').hide();
+    $('#writeAnnouncement').hide();
+    $('#createPoll').toggle('slide',{direction:'up'},500);
+  });
 });
--- php/newsfeed.php
+++ php/newsfeed.php
@@ -55,24 +55,59 @@
       <div id="newsfeed">
         <!-- ONLY VISIBLE FOR EXEC -->
         <div id="newsfeedMenu" class="row">
-          <button class="col-4 text-center">
+          <button class="col-4 text-center" id="openAnnouncementBoxBtn">
             <p><i class="fa fa-bullhorn" aria-hidden="true"></i></p>
             <p>Write</p>
             <p>Announcement</p>
           </button>
 
-          <button class="col-4 text-center">
+          <button class="col-4 text-center" id="openPictureBoxBtn">
             <p><i class="fa fa-picture-o" aria-hidden="true"></i></p>
             <p>Upload</p>
             <p>Picture</p>
           </button>
 
-          <button class="col-4 text-center">
+          <button class="col-4 text-center" id="openPollBoxBtn">
             <p><i class="fa fa-bar-chart" aria-hidden="true"></i></p>
             <p>Create</p>
             <p>Poll</p>
           </button>
         </div>
+
+          <!-- WRITE ANNOUNCEMENT BOX -->
+          <div class="newsfeedAddPost row" id="writeAnnouncement">
+            <textarea name="announcementTextarea" id="announcementTextarea" placeholder="Write Announcement..." class="form-control form-control-lg"></textarea>
+            <div class="col-12">
+              <button type="button" class="btn btn-lg float-right" id="postAnnouncementBtn">Post</button>
+            </div>
+            <div class="clear"></div>
+          </div>
+ 
+          <!-- UPLOAD PICTURE BOX -->
+          <div class="newsfeedAddPost row" id="uploadPicture">
+            <div class="custom-file">
+              <input type="file" class="custom-file-input" id="customFile">
+              <label class="custom-file-label" for="customFile">Choose picture</label>
+            </div>
+            <textarea name="pictureTextarea" id="pictureTextarea" cols="30" rows="2" placeholder="Description" class="form-control form-control-lg"></textarea>
+            <div class="col-12">
+              <button type="button" class="float-right btn btn-lg" id="postPictureBtn">Post</button>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+          <!-- CREATE POLL BOX -->
+          <div class="newsfeedAddPost row" id="createPoll">
+            <input type="text" class="form-control form-control-lg" id="pollQuestion" placeholder="Poll Question">
+            <input type="text" class="form-control form-control-lg" id="pollOptionInput" placeholder="Poll Option">
+            <textarea name="pictureTextarea" id="pollTextarea" cols="30" rows="2" placeholder="Description" class="form-control form-control-lg"></textarea>
+            <div class="col-12">
+              <button type="button" class="float-right btn btn-lg" id="postPollBtn">Post</button>
+            </div>
+            <div class="clear"></div>
+          </div>
+
+        
 
         <!-- TEMPLATE FOR NEWSFEED ANOUNCEMENT POSTS -->
         <div class="post">
@@ -117,7 +152,7 @@
         </div>
         <!-- TEMPLATE FOR ANOUNCEMENT POSTS END -->
         
-        <!-- TEMPLATE FOR NEWSFEED ANOUNCEMENT POSTS -->
+        <!-- TEMPLATE FOR NEWSFEED POLL POSTS -->
         <div class="post">
           <div class="postHeader">
             <img src="../img/default.jpeg" alt="no" class="float-left" width="40px" height="40px">
@@ -127,27 +162,64 @@
           <div class="clear"></div>
 
           <div class="postBody">
-          <form action="">
-            <!-- <div class="custom-control custom-radio">
-              <input type="radio" id="customRadio1" name="customRadio" class="custom-control-input">
-              <label class="custom-control-label" for="customRadio1">
-              <div class="progress">
-                <div class="progress-bar bg-warning" role="progressbar" style="width: 75%" aria-valuenow="75" aria-valuemin="0" aria-valuemax="100"></div>
-              </div>
-              </label>
+            <div class="poll">
+              <h6>Poll Question????</h6>
+
+              <!-- FORM FOR THE POLL
+                   SHOULD I ADD A SUBMIT BUTTON?-->
+              <form action="">
+
+              <!-- POLL OPTION TEMPLATE -->
+                <div class="pollOption">
+                  <label class="custom-control custom-radio float-left">
+                    <input id="radio1" name="radio" type="radio" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                  </label>
+                  <div class="progress float-left">
+                    <div class="progress-bar float-left" role="progressbar" style="width: 30%" aria-valuenow="30" aria-valuemin="0" aria-valuemax="100">
+                      <p>Option 1</p>
+                    </div>
+                  </div>
+                  <p class="float-right">3 votes</p>
+                </div>
+                <div class="clear"></div>
+                <!-- END OF POLL OPTION TEMPLATE -->
+
+                <!-- MORE POLL OPTIONS -->
+                <div class="pollOption">
+                  <label class="custom-control custom-radio float-left">
+                    <input id="radio1" name="radio" type="radio" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                  </label>
+                  <div class="progress float-left">
+                    <div class="progress-bar float-left" role="progressbar" style="width: 20%" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100">
+                      <p>Option 2</p>
+                    </div>
+                  </div>
+                  <p class="float-right">2 votes</p>
+                </div>
+                <div class="clear"></div>
+
+                <div class="pollOption">
+                  <label class="custom-control custom-radio float-left">
+                    <input id="radio1" name="radio" type="radio" class="custom-control-input">
+                    <span class="custom-control-indicator"></span>
+                  </label>
+                  <div class="progress float-left">
+                    <div class="progress-bar float-left" role="progressbar" style="width: 50%" aria-valuenow="50" aria-valuemin="0" aria-valuemax="100">
+                      <p>Option 3</p>
+                    </div>
+                  </div>
+                  <p class="float-right">5 votes</p>
+                </div>
+                <div class="clear"></div>
+
+              </form>      
+              <!-- END OF POLL FORM -->
             </div>
-            <div class="custom-control custom-radio">
-              <input type="radio" id="customRadio2" name="customRadio" class="custom-control-input">
-              <label class="custom-control-label" for="customRadio2">Or toggle this other custom radio</label>
-            </div> -->
-            <div class="custom-control custom-checkbox">
-              <input type="checkbox" class="custom-control-input" id="customCheck1">
-              <label class="custom-control-label" for="customCheck1">Check this custom checkbox</label>
-            </div>
-          </form>
             
-            
-            <p>Ham ham hock cow, shank beef ribs pancetta sausage. Beef cow kevin short loin ball tip spare ribs ribeye tail pig meatloaf chicken biltong prosciutto picanha. Tri-tip shankle ham chicken alcatra buffalo frankfurter salami porchetta short ribs tail shank pork chop spare ribs ground round. Doner turducken sausage, turkey sirloin corned beef chicken burgdoggen capicola jowl. Strip steak rump pancetta capicola beef ribs drumstick alcatra tri-tip sirloin corned beef burgdoggen. Pastrami ham bresaola, tri-tip burgdoggen ball tip beef ribs shoulder.</p>
+            <!-- SOME DESCRIPTION TEXT -->
+            <p>Ham ham hock cow, shank beef ribs pancetta sausage. Beef cow kevin short loin ball tip spare ribs ribeye tail pig meatloaf chicken biltong prosciutto picanha. Tri-tip shankle ham chicken alcatra buffalo frankfurter salami porchetta short ribs tail shank pork chop spare ribs ground round.</p>
           </div>
           
           <footer class="postFooter">
@@ -156,9 +228,9 @@
           </footer>
           <div class="clear"></div>
         </div>
-        <!-- TEMPLATE FOR ANOUNCEMENT POSTS END -->
+        <!-- TEMPLATE FOR POLL POSTS END -->
 
-        <!-- TEMPLATE FOR NEWSFEED ANOUNCEMENT POSTS -->
+        <!-- TEMPLATE FOR NEWSFEED IMAGE POSTS -->
         <div class="post">
           <div class="postHeader">
             <img src="../img/default.jpeg" alt="no" class="float-left" width="40px" height="40px">
@@ -178,7 +250,7 @@
           </footer>
           <div class="clear"></div>
         </div>
-        <!-- TEMPLATE FOR ANOUNCEMENT POSTS END -->
+        <!-- TEMPLATE FOR IMAGE POSTS END -->
 
       </div>      
       <?php
@@ -193,6 +265,10 @@
     integrity="sha384-vFJXuSJphROIrBnz7yo7oB41mKfc8JzQZiCq4NCceLEaO4IHwicKwpJf9c9IpFgh" crossorigin="anonymous"></script>
     <script src="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0-beta.2/js/bootstrap.min.js" 
     integrity="sha384-alpBpkh1PFOepccYVYDB4do5UnbKysX5WZXm3XxPqe5iKTfUKjNkCk9SaVuEZflJ" crossorigin="anonymous"></script>
+
+    <!-- jQuery UI -->
+    <script src="https://code.jquery.com/ui/1.12.1/jquery-ui.js"
+    integrity="sha256-T0Vest3yCU7pafRw9r+settMBX6JkKN06dqBnpQ8d30=" crossorigin="anonymous"></script>
 
     <!-- FontAwesome -->
     <script src="https://use.fontawesome.com/74007ae870.js"></script>

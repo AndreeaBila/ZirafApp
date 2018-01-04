23581e658daa22ee9cc69c73e381bcce41b353d9
SpecDI <vlad@herghelegiu.com>
2018-01-04 15:48:17 +0200

    sql export

--- zirafapp_zirafers.sql
+++ zirafapp_zirafers.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Generation Time: Dec 30, 2017 at 03:00 PM
+-- Generation Time: Jan 04, 2018 at 02:46 PM
 -- Server version: 10.1.28-MariaDB
 -- PHP Version: 7.1.11
 
@@ -51,7 +51,13 @@ CREATE TABLE `chats` (
 --
 
 INSERT INTO `chats` (`chatId`, `chatName`, `dateCreated`) VALUES
-(1, 'All Zirafers', '2017-12-24');
+(1, 'All Zirafers', '2017-12-24'),
+(2, 'Test', '2017-12-30'),
+(3, 'Second test', '2017-12-30'),
+(4, 'New', '2017-12-30'),
+(5, 'Tweeks', '2018-01-03'),
+(6, 'Test', '2018-01-03'),
+(7, 'Last Test', '2018-01-03');
 
 -- --------------------------------------------------------
 
@@ -66,6 +72,77 @@ CREATE TABLE `messages` (
   `content` text NOT NULL,
   `dateCreated` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `messages`
+--
+
+INSERT INTO `messages` (`messageId`, `userId`, `chatId`, `content`, `dateCreated`) VALUES
+(20, 85, 1, 'hi there', '2018-01-03 23:47:00'),
+(21, 85, 1, 'this is my second message', '2018-01-03 23:47:00'),
+(22, 86, 1, 'hi vlad', '2018-01-03 23:48:00'),
+(23, 85, 1, 'hi andreea', '2018-01-03 23:48:00'),
+(24, 85, 1, 'how you doin', '2018-01-03 23:48:00'),
+(25, 86, 1, 'i.m good', '2018-01-03 23:48:00'),
+(26, 86, 1, 'what about you', '2018-01-03 23:48:00'),
+(27, 85, 1, 'i\'m good too', '2018-01-03 23:48:00'),
+(28, 85, 1, 'working ', '2018-01-03 23:48:00'),
+(29, 85, 1, 'what are you doing right now?', '2018-01-03 23:49:00'),
+(30, 86, 1, 'nothing much', '2018-01-03 23:49:00'),
+(31, 86, 1, 'just testing the website', '2018-01-03 23:49:00'),
+(32, 86, 1, 'i want to sleep', '2018-01-03 23:49:00'),
+(33, 86, 1, 'but i cant', '2018-01-03 23:49:00'),
+(34, 86, 1, 'cuz i hva to test the website', '2018-01-03 23:50:00'),
+(35, 85, 1, 'oh', '2018-01-03 23:50:00'),
+(36, 85, 1, 'that soubds naspa', '2018-01-03 23:50:00'),
+(37, 85, 1, 'i\'m sorry to hear that', '2018-01-03 23:50:00'),
+(38, 85, 1, 'i think', '2018-01-04 00:27:00'),
+(39, 85, 1, 'i just fixed the messages', '2018-01-04 00:27:00'),
+(40, 86, 1, 'wow', '2018-01-04 00:27:00'),
+(41, 86, 1, 'that woulkd be great', '2018-01-04 00:27:00'),
+(42, 86, 1, 'you surwe it works?', '2018-01-04 00:28:00'),
+(43, 85, 1, 'not much', '2018-01-04 00:28:00'),
+(44, 85, 1, 'but yes', '2018-01-04 00:28:00'),
+(45, 85, 1, 'it seems that the problem was solved', '2018-01-04 00:28:00'),
+(46, 85, 1, 'ill test a lil more tomowrrow', '2018-01-04 00:28:00'),
+(47, 86, 1, 'great', '2018-01-04 00:28:00'),
+(48, 86, 1, 'good to hear it', '2018-01-04 00:28:00'),
+(49, 86, 1, 'and you wanna go to slee[p now?', '2018-01-04 00:28:00'),
+(50, 85, 1, 'yes', '2018-01-04 00:29:00'),
+(51, 85, 1, 'I would love to', '2018-01-04 00:29:00'),
+(52, 85, 1, 'do wyou want to go?', '2018-01-04 00:29:00'),
+(53, 85, 1, 'heeei', '2018-01-04 00:29:00'),
+(54, 86, 1, 'hey', '2018-01-04 00:29:00'),
+(55, 85, 1, 'hey', '2018-01-04 00:29:00'),
+(56, 86, 1, 'yeah', '2018-01-04 00:29:00'),
+(57, 86, 1, 'sorry', '2018-01-04 00:29:00'),
+(58, 86, 1, 'I didnt pay attention', '2018-01-04 00:29:00'),
+(59, 86, 1, 'yes', '2018-01-04 00:30:00'),
+(60, 86, 1, 'I would love to go', '2018-01-04 00:30:00'),
+(61, 85, 1, 'great', '2018-01-04 00:30:00'),
+(62, 87, 1, 'hi guuuuys', '2018-01-04 12:18:00'),
+(63, 87, 1, 'mornin', '2018-01-04 12:18:00'),
+(64, 85, 1, 'mornin marie', '2018-01-04 12:18:00'),
+(65, 85, 1, 'ce faci?', '2018-01-04 12:18:00'),
+(66, 87, 1, ']bine bai', '2018-01-04 12:18:00'),
+(67, 87, 1, 'blana', '2018-01-04 12:18:00'),
+(68, 85, 1, 'perf', '2018-01-04 12:18:00'),
+(69, 87, 1, 'voi?', '2018-01-04 12:18:00'),
+(70, 85, 1, 'bineee', '2018-01-04 12:18:00'),
+(71, 85, 1, 'la calculator', '2018-01-04 12:19:00'),
+(72, 87, 1, 'marfa', '2018-01-04 12:19:00'),
+(73, 85, 1, 'gataaaa', '2018-01-04 12:35:00'),
+(74, 85, 1, 'cred ca am re[arat tot', '2018-01-04 12:35:00'),
+(75, 87, 1, 'blana', '2018-01-04 12:36:00'),
+(76, 87, 1, 'parca merge', '2018-01-04 12:36:00'),
+(77, 87, 1, 'ti se pare ceva ciudat?', '2018-01-04 12:36:00'),
+(78, 85, 1, 'nup', '2018-01-04 12:36:00'),
+(79, 85, 1, 'all seems good', '2018-01-04 12:36:00'),
+(80, 87, 1, 'perf', '2018-01-04 12:36:00'),
+(81, 85, 1, 'yep', '2018-01-04 12:36:00'),
+(82, 85, 1, 'hai ca merge', '2018-01-04 12:37:00'),
+(83, 87, 1, 'ok', '2018-01-04 12:37:00'),
+(84, 87, 1, 'lets do posts', '2018-01-04 12:37:00');
 
 -- --------------------------------------------------------
 
@@ -100,11 +177,20 @@ CREATE TABLE `poll_options` (
 
 CREATE TABLE `posts` (
   `postId` int(11) NOT NULL,
-  `title` varchar(50) NOT NULL,
   `content` text NOT NULL,
-  `likes` int(11) NOT NULL,
   `dateCreated` date NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `posts`
+--
+
+INSERT INTO `posts` (`postId`, `content`, `dateCreated`) VALUES
+(4, 'Hi there\nThsi is vlad', '2018-01-04'),
+(5, 'This is my last post\nGoodbye!', '2018-01-04'),
+(6, 'dasdasdasdas', '2018-01-04'),
+(7, 'asdasdasdasd', '2018-01-04'),
+(8, 'asdasdsadsad\nasdasd\nasdasdasd\nadasdasdasd', '2018-01-04');
 
 -- --------------------------------------------------------
 
@@ -189,9 +275,24 @@ CREATE TABLE `user_chats` (
 --
 
 INSERT INTO `user_chats` (`userId`, `chatId`, `dateAdded`) VALUES
-(85, 1, '2017-12-30'),
-(86, 1, '2017-12-30'),
-(87, 1, '2017-12-30');
+(85, 1, '2018-01-03'),
+(85, 7, '2018-01-03'),
+(86, 1, '2018-01-03'),
+(86, 2, '2017-12-30'),
+(86, 7, '2018-01-03'),
+(87, 1, '2018-01-03'),
+(87, 7, '2018-01-03');
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `user_likes`
+--
+
+CREATE TABLE `user_likes` (
+  `userId` int(11) NOT NULL,
+  `postId` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -213,9 +314,19 @@ CREATE TABLE `user_polls` (
 
 CREATE TABLE `user_posts` (
   `userId` int(11) NOT NULL,
-  `postId` int(11) NOT NULL,
-  `userName` varchar(30) NOT NULL
+  `postId` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Dumping data for table `user_posts`
+--
+
+INSERT INTO `user_posts` (`userId`, `postId`) VALUES
+(85, 4),
+(85, 5),
+(85, 6),
+(85, 7),
+(85, 8);
 
 -- --------------------------------------------------------
 
@@ -299,6 +410,13 @@ ALTER TABLE `user_chats`
   ADD KEY `chatId` (`chatId`);
 
 --
+-- Indexes for table `user_likes`
+--
+ALTER TABLE `user_likes`
+  ADD PRIMARY KEY (`userId`,`postId`),
+  ADD KEY `postId` (`postId`);
+
+--
 -- Indexes for table `user_polls`
 --
 ALTER TABLE `user_polls`
@@ -332,13 +450,13 @@ ALTER TABLE `badges`
 -- AUTO_INCREMENT for table `chats`
 --
 ALTER TABLE `chats`
-  MODIFY `chatId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=43;
+  MODIFY `chatId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
 
 --
 -- AUTO_INCREMENT for table `messages`
 --
 ALTER TABLE `messages`
-  MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=69;
+  MODIFY `messageId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=85;
 
 --
 -- AUTO_INCREMENT for table `polls`
@@ -356,7 +474,7 @@ ALTER TABLE `poll_options`
 -- AUTO_INCREMENT for table `posts`
 --
 ALTER TABLE `posts`
-  MODIFY `postId` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `postId` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=9;
 
 --
 -- AUTO_INCREMENT for table `reviews`
@@ -400,6 +518,13 @@ ALTER TABLE `user_badges`
 ALTER TABLE `user_chats`
   ADD CONSTRAINT `USER_CHAT_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE,
   ADD CONSTRAINT `USER_CHAT_ibfk_2` FOREIGN KEY (`chatId`) REFERENCES `chats` (`chatId`) ON DELETE CASCADE;
+
+--
+-- Constraints for table `user_likes`
+--
+ALTER TABLE `user_likes`
+  ADD CONSTRAINT `user_likes_ibfk_1` FOREIGN KEY (`userId`) REFERENCES `users` (`userId`) ON DELETE CASCADE,
+  ADD CONSTRAINT `user_likes_ibfk_2` FOREIGN KEY (`postId`) REFERENCES `posts` (`postId`) ON DELETE CASCADE;
 
 --
 -- Constraints for table `user_posts`
